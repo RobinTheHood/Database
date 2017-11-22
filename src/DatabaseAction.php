e95cfa3015e27effcd4b4e2d9f74df8cbc4b5de7
Robin Wieschendorf <mail@robinwieschendorf.de>
2017-11-22 19:14:06 +0100

    DatabaseAction added

---
+++ src/DatabaseAction.php
@@ -0,0 +1,44 @@
+<?php
+namespace DatabaseAddOn;
+
+use Database\Database;
+use Debug\Debug;
+
+class DatabaseAction
+{
+    public static function execute($sql, $values = [])
+    {
+        $pdo = Database::getConnection();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($values);
+
+        if($stmt->errorCode() != 0) {
+            Debug::error('SQL-ERROR: ' . $stmt->errorInfo()[2]);
+            die();
+        }
+
+        return self::lastInsertId();
+    }
+
+    public static function query($sql, $values = [])
+    {
+        $pdo = Database::getConnection();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($values);
+
+        if($stmt->errorCode() == 0) {
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } else {
+            Debug::error('SQL-ERROR: ' . $stmt->errorInfo()[2]);
+            die();
+        }
+
+        return $result;
+    }
+
+    public static function lastInsertId()
+    {
+        $pdo = Database::getConnection();
+        return $pdo->lastInsertId();
+    }
+}
